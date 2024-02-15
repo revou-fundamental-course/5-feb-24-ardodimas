efdaf2a02664ce73ab4a27853b27c109527db9a5
ardodimas <singaraja113@gmail.com>
2024-02-15 08:24:37 +0800

    Project Finish

--- js/script.js
+++ js/script.js
@@ -8,7 +8,9 @@ function konversi () {
 
         var caraKalkulasi = "Fahrenheit = (" + suhuCelcius + "°C * 9/5) + 32 = " + suhuFahrenheit + "°F ";
         document.getElementById("caraKalkulasi").value = caraKalkulasi;
-    }else {
+
+    }
+    else {
         document.getElementById("result").innerHTML = "Masukkan suhu dalam angka";
     }
 }
@@ -17,4 +19,22 @@ function reset() {
     document.getElementById("inputCelcius").value = "";
     document.getElementById("result").value = "";
     document.getElementById("caraKalkulasi").value = "";
+}
+
+function reverse() {
+    var inputFahrenheit = document.getElementById("result").value;
+    var suhuFahrenheit = parseFloat(inputFahrenheit);
+
+    if (!isNaN(suhuFahrenheit)) {
+        var suhuCelcius = (suhuFahrenheit - 32) * 5/9;
+        document.getElementById("inputCelcius").value = suhuCelcius.toFixed(2);
+        
+        var caraKalkulasi = "Celcius = ("+ suhuFahrenheit +" - 32) * 5/9 = "+ suhuCelcius.toFixed(2) +"°C";
+        document.getElementById("caraKalkulasi").value = caraKalkulasi;
+
+        document.getElementById("result").value = suhuFahrenheit.toFixed(2);
+    } else {
+        document.getElementById("inputCelcius").value = "";
+        document.getElementById("caraKalkulasi").value = "";
+    }
 }
